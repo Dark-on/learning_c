25858b78ba855d654caedcdaa0211c7ddefc704c
Dark-on <Sipulka.Dr@gmail.com>
2022-10-11 00:09:17 +0200

    Add math_func

---
+++ math_func/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "math.h"
+
+int main()
+{
+    printf("100 + 20 = %d", add(100, 20));
+    printf("100 - 20 = %d", sub(100, 20));
+    // printf("100 * 20 = %d", mul(100, 20));
+    // printf("100 / 20 = %d", div(100, 20));
+    printf("\n\nFinish\n\n");
+    return 0;
+}
---
+++ math_func/math.h
@@ -0,0 +1,9 @@
+
+#ifndef MATH_H_
+#define MATH_H_
+
+int add(int num1, int num2);
+int sub(int num1, int num2);
+// int mul(int num1, int num2);
+// int div(int num1, int num2);
+#endif /* MATH_H_ */
